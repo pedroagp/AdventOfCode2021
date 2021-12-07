cc8fa5e8d69280b1e15f23f5f986738b98395b23
Pedro Pita <pedro.pita@outsystems.com>
2021-12-07 07:07:50 +0000

    day7

--- Day07/Day07.cs
+++ Day07/Day07.cs
@@ -6,22 +6,52 @@ using System.Linq;
 
 namespace AdventOfCode2021.Challenges {
     internal class Day07 : Challenge {
-        private readonly string[] input;
+        private readonly int[] input;
 
         public Day07() {
             if (input is null) {
-                input = File.ReadAllLines(Path.Combine(this.GetType().Name, Constants.DefaultFileName));
+                input = File.ReadAllLines(Path.Combine(this.GetType().Name, Constants.DefaultFileName))[0].Split(',').Select(int.Parse).ToArray();
+                Array.Sort(input);
             }
         }
 
         public override string FirstResult() {
-            
-            return default;
+            var cost = int.MaxValue;
+
+            for (int i = input[0]; i < input[input.Length - 1]; i++) {
+                var tmp = 0;
+                for(int j = 0; j < input.Length; j++){
+                    tmp += Math.Abs(input[j]-i);
+                }
+                
+                if (tmp < cost) cost = tmp;
+            }
+     
+
+            return $"{cost}";
         }
 
-        public override string SecondResult() {
 
-            return default;
+        public override string SecondResult() {
+            var cost = int.MaxValue;
+            int[] costs = new int[input[input.Length-1]+1];
+            
+            costs[0] = 0;
+            costs[1] = 1;
+            for (int i = 2; i <= input[input.Length - 1]; i++) {
+                costs[i] = i + costs[i - 1];
+            }
+
+            for (int i = input[0]; i < input[input.Length - 1]; i++) {
+                var tmp = 0;
+                for (int j = 0; j < input.Length; j++) {
+                    tmp += costs[Math.Abs(input[j] - i)];
+                }
+                if (tmp < cost) cost = tmp;
+            }
+
+
+            return $"{cost}";
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using AdventOfCode2021.Common;
 using System.Collections.Generic;
 
-var challenges = new List<Challenge> { new Day01(), new Day02(), new Day03(), new Day04(), new Day05(), new Day06() };
+var challenges = new List<Challenge> { new Day01(), new Day02(), new Day03(), new Day04(), new Day05(), new Day06(), new Day07() };
 
 foreach (var challenge in challenges) {
     challenge.Run();
