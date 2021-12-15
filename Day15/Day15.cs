abfa8c05489d3f54ffa7a5f6d0ba93bc1d831d60
Pedro Pita <pedro.pita@outsystems.com>
2021-12-15 09:15:44 +0000

    day 15 and finally stopped output every other day because todays 2nd result took a while.

--- Day15/Day15.cs
+++ Day15/Day15.cs
@@ -6,17 +6,89 @@ using System.Linq;
 
 namespace AdventOfCode2021.Challenges {
     internal class Day15 : Challenge {
-        public Day15(bool testInput = false) : base(testInput) { }
+        private struct Coord {
+            public int x; public int y;
+
+            public Coord(int x, int y) {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        private Dictionary<Coord, int> graph;
+        private readonly int[][] intput;
+        public Day15(bool testInput = false) : base(testInput) {
+            intput = base.input.Select(x => x).Select(x => x.ToCharArray().Select(x => (int)char.GetNumericValue(x)).ToArray()).ToArray();
+            graph = new Dictionary<Coord, int>();
+            for (int i = 0; i < intput.Length; i++) {
+                for (int j = 0; j < intput[i].Length; j++) {
+                    graph[new(j, i)] = intput[j][i];
+                }
+            }
+        }
 
         public override string FirstResult() {
-
-            return $"";
+            return $"{Dijkstra(graph)}";
         }
 
 
         public override string SecondResult() {
+            BlowUpGraph(5);
+            return $"{Dijkstra(graph)}";
+        }
 
-            return $"";
+        private void BlowUpGraph(int size) {
+            int[][] newIntput = new int[size * intput.Length][];
+            for (int i = 0; i < size * intput.Length; i++) {
+                newIntput[i] = new int[size * intput[0].Length];
+            }
+            for (int i = 0; i < newIntput.Length; i++) {
+                for (int j = 0; j < newIntput[i].Length; j++) {
+                    var dist = (i / intput.Length) + (j / intput[0].Length);
+                    var newValue = intput[j % intput[0].Length][i % intput.Length] + dist - 1;
+                    newIntput[j][i] =  (newValue % 9) + 1;
+                }
+            }
+            graph = new Dictionary<Coord, int>();
+            for (int i = 0; i < newIntput.Length; i++) {
+                for (int j = 0; j < newIntput[i].Length; j++) {
+                    graph[new(j, i)] = newIntput[j][i];
+                }
+            }
+        }
+
+        int Dijkstra(Dictionary<Coord, int> graph) {
+            var start = new Coord(0, 0);
+            var end = new Coord(graph.Keys.MaxBy(p => p.x).x, graph.Keys.MaxBy(p => p.y).y);
+
+            var queue = new PriorityQueue<Coord, int>();
+            var pathCosts = new Dictionary<Coord, int> {
+                [start] = 0
+            };
+
+            queue.Enqueue(start, 0);
+
+            while (queue.Count > 0) {
+                var p = queue.Dequeue();
+
+                foreach (var n in borders(p)) {
+                    if (graph.ContainsKey(n) && !pathCosts.ContainsKey(n)) {
+                        var cost = pathCosts[p] + graph[n];
+                        pathCosts[n] = cost;
+                        queue.Enqueue(n, cost);
+                    }
+                }
+            }
+
+            return pathCosts[end];
+        }
+        
+        Coord[] borders(Coord point) {
+            return new[] {
+                   new Coord(point.x+1, point.y),
+                   new Coord(point.x-1, point.y),
+                   new Coord(point.x, point.y+1),
+                   new Coord(point.x, point.y-1),
+            };
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,21 +3,22 @@ using AdventOfCode2021.Common;
 using System.Collections.Generic;
 
 var challenges = new List<Challenge> { 
-    new Day01(), 
-    new Day02(), 
-    new Day03(), 
-    new Day04(), 
-    new Day05(), 
-    new Day06(), 
-    new Day07(), 
-    new Day08(), 
-    new Day09(), 
-    new Day10(), 
-    new Day11(),
-    new Day12(),
-    new Day13(),
-    new Day14(),
-    new Day15(true),
+    //new Day01(), 
+    //new Day02(), 
+    //new Day03(), 
+    //new Day04(), 
+    //new Day05(), 
+    //new Day06(), 
+    //new Day07(), 
+    //new Day08(), 
+    //new Day09(), 
+    //new Day10(), 
+    //new Day11(),
+    //new Day12(),
+    //new Day13(),
+    //new Day14(),
+    //new Day15(),
+    new Day16(true),
 };
 
 foreach (var challenge in challenges) {
